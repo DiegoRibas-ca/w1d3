1014769551267048b9ed250c8e7a636cf04b0b39
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-01-17 17:16:08 +0000

    creating object and count keys

---
+++ counting/counting.js
@@ -0,0 +1,14 @@
+  var countObjc = {}
+function countLetters (string) {
+ var arrayPhrase = string.toLowerCase().split(' ').join("").split('');
+  for (var i = 0; i < arrayPhrase.length; i++) {
+    if (countObjc[arrayPhrase[i]]) {
+      countObjc[arrayPhrase[i]] += 1;
+    } else {
+      countObjc[arrayPhrase[i]] = 1
+    }
+  }
+  return countObjc;
+}
+
+console.log(countLetters("Thiago e Diego"))
